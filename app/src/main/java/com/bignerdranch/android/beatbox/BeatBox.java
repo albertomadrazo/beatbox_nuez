462e24a14107231861c840caf85892bb8688b363
unknown <it04@IT04.FESA.com>
2017-01-06 13:33:13 -0600

    Crea metodo loadSounds() y hace una instancia de BeatBox en BeatBoxFragment.

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -3,6 +3,9 @@ package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
@@ -12,5 +15,17 @@ public class BeatBox {
 
     public BeatBox(Context context){
         mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds(){
+        String[] soundNames;
+        try{
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe){
+            Log.e(TAG, "Could not list assets", ioe);
+            return;
+        }
     }
 }
